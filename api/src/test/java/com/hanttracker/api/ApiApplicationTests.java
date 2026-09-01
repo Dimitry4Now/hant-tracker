d1ef1d8f95b28c7dd7dcf393ba6e3ef985bc518f
Dimitar Betinski <dimitarbetinski@gmail.com>
2026-09-01 11:38:38 +0200

    Initial scaffold: design, frontend (Angular), api (Spring Boot)

---
+++ api/src/test/java/com/hanttracker/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanttracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
